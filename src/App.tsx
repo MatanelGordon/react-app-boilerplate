6d804bef92e58f2eb0d0162094cdb2ab12dd353f
Matanel Mac <mmgordon.matanel@gmail.com>
2023-07-07 16:49:11 +0300

    content boilerplate changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -8,12 +8,8 @@ function App() {
 	return (
 		<div className="App">
 			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src="/vite.svg" className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://reactjs.org" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
+				<img src="/vite.svg" className="logo vite" alt="Vite logo" />
+				<img src={reactLogo} className="logo react" alt="React logo" />
 			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
@@ -22,7 +18,6 @@ function App() {
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
 			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
 		</div>
 	);
 }
