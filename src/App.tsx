f4d15d26285100c20bc8ba3f224ee3d0a55d7f45
Matanel Mac <mmgordon.matanel@gmail.com>
2023-07-07 21:04:33 +0300

    Changed app with arrow function

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { FC, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.scss';
 
-function App() {
+const App: FC = () => {
 	const [count, setCount] = useState(0);
 
 	return (
